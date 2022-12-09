550c926c1475ab556df63d700b10bdf753face96
Icemourne <Cboweris@gmail.com>
2022-12-09 14:39:26 +0200

    clean up

--- converter/livePerkInterface.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// If you see "Optional" then it is optional and probably not included by default
-// If something is not clear just ask if you think you can improve explanation make PR
-Object.defineProperty(exports, "__esModule", { value: true });
