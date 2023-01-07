bb2334946c80fd772780d6c8a6eacdd8e59ade42
Icemourne <Cboweris@gmail.com>
2023-01-07 17:01:16 +0200

    added more exports

--- converter/index.d.ts
+++ converter/index.d.ts
@@ -1,7 +1,10 @@
+import { extractTitles } from './src/converterFunctions/extractTitles';
 import { getDataFromPerk } from './src/converterFunctions/getDataFromPerk';
+import { getVariables } from './src/converterFunctions/loadVariables';
 import { converterSettings, getSettings } from './src/converterSettings';
+import { statNames, weaponTypes } from './src/data';
 import { descriptionConverter } from './src/descriptionConverter';
 import { descriptionFilter } from './src/descriptionFilter';
-import { Database, DescriptionData, IntermediatePerk, PerkTypes } from './src/interfaces';
+import { CellContent, Database, DescriptionData, DescriptionLine, IntermediatePerk, LinesContent, PerkTypes, RowContent, Stat, TableLine } from './src/interfaces';
 import { Languages, LivePerk, StatNames, WeaponTypes } from './src/livePerkInterface';
-export { descriptionConverter, getSettings, getDataFromPerk, descriptionFilter, converterSettings, LivePerk, Database, Languages, PerkTypes, StatNames, WeaponTypes, DescriptionData, IntermediatePerk };
+export { descriptionConverter, getSettings, getDataFromPerk, descriptionFilter, extractTitles, getVariables, converterSettings, weaponTypes, statNames, LivePerk, Database, Languages, PerkTypes, StatNames, WeaponTypes, DescriptionData, IntermediatePerk, Stat, LinesContent, CellContent, RowContent, TableLine, DescriptionLine };
--- index.ts
+++ index.ts
@@ -1,8 +1,22 @@
+import { extractTitles } from './src/converterFunctions/extractTitles'
 import { getDataFromPerk } from './src/converterFunctions/getDataFromPerk'
+import { getVariables } from './src/converterFunctions/loadVariables'
 import { converterSettings, getSettings } from './src/converterSettings'
+import { statNames, weaponTypes } from './src/data'
 import { descriptionConverter } from './src/descriptionConverter'
 import { descriptionFilter } from './src/descriptionFilter'
-import { Database, DescriptionData, IntermediatePerk, PerkTypes } from './src/interfaces'
+import {
+   CellContent,
+   Database,
+   DescriptionData,
+   DescriptionLine,
+   IntermediatePerk,
+   LinesContent,
+   PerkTypes,
+   RowContent,
+   Stat,
+   TableLine
+} from './src/interfaces'
 import { Languages, LivePerk, StatNames, WeaponTypes } from './src/livePerkInterface'
 
 export {
@@ -10,7 +24,11 @@ export {
    getSettings,
    getDataFromPerk,
    descriptionFilter,
+   extractTitles,
+   getVariables,
    converterSettings,
+   weaponTypes,
+   statNames,
    LivePerk,
    Database,
    Languages,
@@ -18,6 +36,12 @@ export {
    StatNames,
    WeaponTypes,
    DescriptionData,
-   IntermediatePerk
+   IntermediatePerk,
+   Stat,
+   // ---
+   LinesContent,
+   CellContent,
+   RowContent,
+   TableLine,
+   DescriptionLine
 }
-
