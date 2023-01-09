7cfc7e08144b0d0a37c0dacdf6880360466df4bd
Icemourne <Cboweris@gmail.com>
2023-01-09 19:04:42 +0200

    fixed incorect linked perk type

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -46,7 +46,7 @@ export interface IntermediatePerk {
    uploadedBy: string
    type: PerkTypes
    importStatsFrom?: number
-   linking?: {
+   linkedWith?: {
       [key in PossiblePerkLinks]?: number
    }
    updateTracker: {
